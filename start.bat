41bbc091ae84fa5fe9e6d9cf17bb22e1872dd2c7
kimbumyun98 <62227254+kimbumyun98@users.noreply.github.com>
2020-03-21 23:23:41 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
+import os
+
 pip install pyqt5
+
+os.system('@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"')
+os.system('choco install git')
 git clone https://github.com/volatilityfoundation/volatility3.git
 mkdir plugin\init
